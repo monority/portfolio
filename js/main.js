e1bf459fcd22088c8a5e4d0be34ce881a109442d
Ronan C <126070278+monority@users.noreply.github.com>
2023-06-05 15:46:39 +0200

    Fix couleur

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// Main JS // 
+// Main JS //
+const navLinks = document.querySelectorAll("header nav ul li");
+const contents = document.querySelectorAll("section");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+    });
+    link.classList.add("active");
+  });
+});

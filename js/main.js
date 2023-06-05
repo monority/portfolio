36b4259472b8b758cf6cf571dc6345c45eb5f9b7
monority <chenu.pro@gmail.com>
2023-06-05 22:19:18 +0200

    Commit fix couleur et architecture

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // Main JS //
-const navLinks = document.querySelectorAll("header nav ul li");
-const contents = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".wrapperLi ul li");
 
 navLinks.forEach(link => {
   link.addEventListener("click", (e) => {
+    console.log("clicked")
     navLinks.forEach(link => {
       link.classList.remove("active");
     });
